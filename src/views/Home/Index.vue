7ae192adae8e44fa8835ecd3ea985a906dbea593
valzza <valzadalipi2@gmail.com>
2025-03-14 23:07:13 +0100

    Heqja e greeting msg

--- src/views/Home/Index.vue
+++ src/views/Home/Index.vue
@@ -53,11 +53,6 @@ onMounted(async () => {
 <template>
       <Spinner v-if="showSpinner" />
 
-      <!-- Greeting message -->
-      <div class="mb-4 text-3xl font-bold text-gray-800">
-        Hello, Valza
-      </div>
-
       <!-- Statistics Section -->
       <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
         <div class="p-4 bg-white shadow rounded-lg text-center">
